6f85402cef9d85908edd025c851fc0fd36fca4a4
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 16:41:43 -0400

    xuyisen__diff__id_10

--- cloudinit/distros/__init__.py
+++ cloudinit/distros/__init__.py
@@ -151,11 +151,11 @@ class Distro(persistence.CloudInitPickleMixin, metaclass=abc.ABCMeta):
     # Directory where the distro stores their DHCP leases.
     # The children classes should override this with their dhcp leases
     # directory
-    dhclient_lease_directory: str | None = None
+    dhclient_lease_directory: Optional[str] = None
     # A regex to match DHCP lease file(s)
     # The children classes should override this with a regex matching
     # their lease file name format
-    dhclient_lease_file_regex: str | None = None
+    dhclient_lease_file_regex: Optional[str] = None
 
     def __init__(self, name, cfg, paths):
         self._paths = paths
